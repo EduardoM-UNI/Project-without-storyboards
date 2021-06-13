1a27247f030759eedeea7297931efc0c38ce7f50
Eduardo <edu.wap2@gmail.com>
2021-06-13 12:25:15 +0200

    First Commit

---
+++ Project Without StoryBorads/Project Without StoryBorads/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project Without StoryBorads
+//
+//  Created by Eduardo Monroy Husillos on 13/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
